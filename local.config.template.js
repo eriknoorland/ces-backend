1ee80a001168146b92c41425deb99715c6345b06
Erik Noorland <mail@eriknoorland.nl>
2017-04-22 18:12:01 +0200

    Update song GET request

--- local.config.template.js
+++ local.config.template.js
@@ -1,4 +1,5 @@
 module.exports =  {
   staticDir: '',
-  playlistsDir: ''
+  playlistsDir: '',
+  musicDir: ''
 }
--- routes/songs.js
+++ routes/songs.js
@@ -1,8 +1,30 @@
-var express = require('express')
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const config = require('./../local.config');
 
-router.get('/:id', function(request, response) {
-  // request.params.id;
+router.get('/:playlist/:id', function(request, response) {
+  try {
+    const playlistName = `${request.params.playlist}.json`;
+    const playlistPath = `${config.playlistsDir}${playlistName}`;
+    const playlistFile = fs.readFileSync(playlistPath);
+    const playlist = JSON.parse(playlistFile);
+    const song = playlist.songs.filter(song => song.id === request.params.id)[0];
+    const songPath = `${config.musicDir}${song.location}`;
+
+    response.set({'Content-Type': 'audio/mpeg'});
+
+    try {
+      let readStream = fs.createReadStream(songPath);
+      readStream.pipe(response);
+    }
+    catch(error) {
+      response.status(404).send('Song not found!');
+    }
+  }
+  catch(error) {
+    response.status(404).send('Playlist not found!');
+  }
 });
 
 module.exports = router;
